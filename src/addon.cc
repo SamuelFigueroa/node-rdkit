9a5d614b8182069a721c8c164d58837f62743ef0
Samuel Figueroa-Lazu <sammylazu@gmail.com>
2018-12-06 12:25:40 -0600

    Check for null pointer in SmilesToMolBlock

--- src/addon.cc
+++ src/addon.cc
@@ -4,7 +4,8 @@
 #include <GraphMol/SmilesParse/SmilesParse.h>
 #include <RDGeneral/FileParseException.h>
 #include <GraphMol/Descriptors/MolDescriptors.h>
-
+#include <GraphMol/ChemTransforms/ChemTransforms.h>
+#include <GraphMol/MolOps.h>
 using namespace std;
 
 Napi::Value MolBlockToSmiles(const Napi::CallbackInfo& info) {
@@ -30,9 +31,9 @@ Napi::Value MolBlockToSmiles(const Napi::CallbackInfo& info) {
   }
 
   string smiles = "";
-  if (m != NULL) {
+  if (m != nullptr) {
       try {
-          smiles = RDKit::MolToSmiles(*m, true);
+          smiles = RDKit::MolToSmiles(*m);
       } catch (exception &e) {
           Napi::TypeError::New(env, e.what()).ThrowAsJavaScriptException();
           return env.Null();
@@ -64,9 +65,17 @@ Napi::Value SmilesToMolBlock(const Napi::CallbackInfo& info) {
       return env.Null();
   }
 
-  Napi::String mol_block = Napi::String::New(env, RDKit::MolToMolBlock(*m) );
-
-  return mol_block;
+  string molblock = "";
+  if (m != nullptr) {
+      try {
+          molblock = RDKit::MolToMolBlock(*m);
+      } catch (exception &e) {
+          Napi::TypeError::New(env, e.what()).ThrowAsJavaScriptException();
+          return env.Null();
+      }
+  }
+  Napi::String napi_molblock = Napi::String::New(env, molblock);
+  return napi_molblock;
 }
 
 Napi::Value MolWtFromSmiles(const Napi::CallbackInfo& info) {
@@ -96,6 +105,114 @@ Napi::Value MolWtFromSmiles(const Napi::CallbackInfo& info) {
   return mol_wt;
 }
 
+//RDKit::ROMOL_SPTR _applyPattern(RDKit::ROMOL_SPTR m, RDKit::ROMOL_SPTR salt, bool notEverything) {
+
+//    unsigned int nAts = m->getNumAtoms();
+//    if(nAts == 0)
+//        return m;
+//    RDKit::ROMOL_SPTR res(m);
+
+//    RDKit::ROMOL_SPTR t(RDKit::deleteSubstructs(*res, *salt, true, true));
+//    if(t == nullptr || (notEverything && t->getNumAtoms() == 0))
+//        return res;
+//    res = t;
+//    while (res->getNumAtoms() && nAts > res->getNumAtoms()) {
+//        nAts = res->getNumAtoms();
+//        RDKit::ROMOL_SPTR temp(RDKit::deleteSubstructs(*res, *salt, true, true));
+//        if(notEverything && temp->getNumAtoms() == 0)
+//            break;
+//        res = temp;
+//    }
+//    return res;
+
+//}
+
+//Napi::Value RemoveSaltsFromSmiles(const Napi::CallbackInfo& info) {
+//  Napi::Env env = info.Env();
+
+//  if (info.Length() < 2) {
+//    Napi::TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
+//    return env.Null();
+//  }
+
+//  if (!info[0].IsString()) {
+//    Napi::TypeError::New(env, "First argument must be a string.").ThrowAsJavaScriptException();
+//    return env.Null();
+//  }
+
+//  if (!info[1].IsArray()) {
+//    Napi::TypeError::New(env, "Second argument must be an array of strings.").ThrowAsJavaScriptException();
+//    return env.Null();
+//  }
+
+//  string smiles = info[0].As<Napi::String>();
+
+//  vector <string> salts;
+//  for(unsigned int i = 0; i < info[1].As<Napi::Array>().Length(); i++)
+//      salts.push_back(info[1].As<Napi::Array>().Get(i).As<Napi::String>());
+
+//  bool dontRemoveEverything = !info[2].IsUndefined() ? info[2].As<Napi::Boolean>() : true;
+
+//  RDKit::ROMOL_SPTR mol;
+//  try {
+//      mol = RDKit::ROMOL_SPTR(RDKit::SmilesToMol(smiles));
+//  } catch (RDKit::SmilesParseException &e) {
+//      Napi::TypeError::New(env, e.message()).ThrowAsJavaScriptException();
+//      return env.Null();
+//  }
+
+//  vector<string> removed(0);
+
+//  Napi::Object result = Napi::Object::New(env);
+//  result.Set(Napi::String::New(env, "molecule"), smiles);
+//  result.Set(Napi::String::New(env, "removed"), Napi::Array::New(env));
+
+//  if(dontRemoveEverything && RDKit::MolOps::getMolFrags(*mol).size() <= 1) {
+//      return result;
+//  }
+
+//  bool modified = false;
+//  unsigned int numAtoms = mol->getNumAtoms();
+//  for (unsigned int i = 0; i < salts.size(); i++) {
+//      RDKit::ROMOL_SPTR salt;
+//      try {
+//          salt = RDKit::ROMOL_SPTR(RDKit::SmilesToMol(salts[i]));
+//      } catch (RDKit::SmilesParseException &e) {
+//          Napi::TypeError::New(env, e.message()).ThrowAsJavaScriptException();
+//          return env.Null();
+//      }
+//      mol = _applyPattern(mol, salt, dontRemoveEverything);
+//      if(numAtoms != mol->getNumAtoms()) {
+//          numAtoms = mol->getNumAtoms();
+//          modified = true;
+//          removed.push_back(salts[i]);
+//          if(dontRemoveEverything && RDKit::MolOps::getMolFrags(*mol).size() <= 1)
+//              break;
+//      }
+//  }
+//  if (modified && mol->getNumAtoms() > 0) {
+//      RDKit::RWMOL_SPTR desalted( new RDKit::RWMol(*mol, true));
+//      RDKit::MolOps::sanitizeMol(*desalted);
+//      string smiles = "";
+//      try {
+//          smiles = RDKit::MolToSmiles(*desalted);
+//      } catch (exception &e) {
+//          Napi::TypeError::New(env, e.what()).ThrowAsJavaScriptException();
+//          return env.Null();
+//      }
+
+//      Napi::Array removed_salts = Napi::Array::New(env, removed.size());
+//      for(unsigned int i = 0; i != removed.size(); i++) {
+//          removed_salts[i] = Napi::String::New(env, removed[i]);
+//      }
+//      result.Set(Napi::String::New(env, "molecule"), smiles);
+//      result.Set(Napi::String::New(env, "removed"), removed_salts);
+//  }
+
+//  return result;
+
+//}
+
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
   exports.Set(Napi::String::New(env, "molBlockToSmiles"),
               Napi::Function::New(env, MolBlockToSmiles));
@@ -103,6 +220,8 @@ Napi::Object Init(Napi::Env env, Napi::Object exports) {
               Napi::Function::New(env, SmilesToMolBlock));
   exports.Set(Napi::String::New(env, "molWtFromSmiles"),
               Napi::Function::New(env, MolWtFromSmiles));
+//  exports.Set(Napi::String::New(env, "removeSaltsFromSmiles"),
+//              Napi::Function::New(env, RemoveSaltsFromSmiles));
   return exports;
 }
 
--- tests/runAddon.js
+++ tests/runAddon.js
@@ -1,46 +1,50 @@
 const rdkit = require("bindings")("node-rdkit");
 
-const benzene_mol_block = `
-     CWRITER311071816342D
-
-  6  6  0  0  0  0  0  0  0  0999 V2000
-    3.6046    6.0097    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
-    4.4706    5.5097    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
-    5.3366    6.0097    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
-    5.3366    7.0097    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
-    4.4706    7.5097    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
-    3.6046    7.0097    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+const test_molblock = `
+  ZLAB311291814222D
+http://medchemprdimsap.cc.ku.edu
+  8  7  0  0  0  0  0  0  0  0999 V2000
+    5.0972   -4.9216    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    5.9633   -5.4216    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    6.8293   -4.9216    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    6.8293   -3.9216    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    5.9633   -3.4216    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    5.0972   -3.9216    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    5.9633   -2.4216    0.0000 N   0  0  0  0  0  0  0  0  0  0  0  0
+    8.4744   -3.8269    0.0000 Cl  0  0  0  0  0  0  0  0  0  0  0  0
   1  2  2  0  0  0
   2  3  1  0  0  0
   3  4  2  0  0  0
   4  5  1  0  0  0
   5  6  2  0  0  0
   6  1  1  0  0  0
+  5  7  1  0  0  0
+M  CHG  2   7   1   8  -1
 M  END`;
 
-const empty_mol_block = `
-     CWRITER311161812292D
+// const empty_mol_block = `
+//      CWRITER311161812292D
+//
+//   0  0  0  0  0  0  0  0  0  0999 V2000
+// M  END`;
 
-  0  0  0  0  0  0  0  0  0  0999 V2000
-M  END`;
-
-const benzene_smiles = rdkit.molBlockToSmiles(benzene_mol_block);
-console.log('Benzene: ', benzene_smiles);
-
-const empty_mol_smiles = rdkit.molBlockToSmiles(empty_mol_block);
-console.log('Empty mol: ', empty_mol_smiles);
-
-const empty_string_smiles = rdkit.molBlockToSmiles('');
-console.log('Empty string: ', empty_string_smiles);
-
-const benzene_molblock = rdkit.smilesToMolBlock(benzene_smiles);
-console.log('Benzene Mol Block: ', benzene_molblock);
-
-const empty_molblock = rdkit.smilesToMolBlock('');
-console.log('Empty Mol Block: ', empty_molblock);
-
-const benzene_molwt = rdkit.molWtFromSmiles(benzene_smiles);
-console.log('Mol Wt: ', benzene_molwt);
-
-const empty_molwt = rdkit.molWtFromSmiles('');
-console.log('Empty Mol Wt: ', empty_molwt);
+// const benzene_smiles = rdkit.molBlockToSmiles(benzene_mol_block);
+// console.log('Benzene: ', benzene_smiles);
+//
+// const empty_mol_smiles = rdkit.molBlockToSmiles(empty_mol_block);
+// console.log('Empty mol: ', empty_mol_smiles);
+//
+// const empty_string_smiles = rdkit.molBlockToSmiles('');
+// console.log('Empty string: ', empty_string_smiles);
+//
+// const benzene_molblock = rdkit.smilesToMolBlock(benzene_smiles);
+// console.log('Benzene Mol Block: ', benzene_molblock);
+//
+// const empty_molblock = rdkit.smilesToMolBlock('');
+// console.log('Empty Mol Block: ', empty_molblock);
+//
+// const benzene_molwt = rdkit.molWtFromSmiles(benzene_smiles);
+// console.log('Mol Wt: ', benzene_molwt);
+//
+// const empty_molwt = rdkit.molWtFromSmiles('');
+// console.log('Empty Mol Wt: ', empty_molwt);
